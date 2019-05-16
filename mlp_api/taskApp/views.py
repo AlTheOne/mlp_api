dac0245beda3531470ef40b9f18a71fbc3f3341a
Yura Pogorelov <pyurik44@gmail.com>
2019-05-16 23:28:43 +0300

    bugs

--- mlp_api/taskApp/views.py
+++ mlp_api/taskApp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Task.objects.all()
+        return queryset
+
+    def list(self, *args, **kwargs):
+        # project_slug записывается в словарь **kwargs
+        queryset = Task.objects.filter(task_project__slug=kwargs.get('project_slug'))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
