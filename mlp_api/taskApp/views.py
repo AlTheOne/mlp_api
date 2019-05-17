f6d93ea675db8638bd6fa7c9509e90b47ede4423
Yurik <pyurik44@gmail.com>
2019-05-17 19:13:02 +0300

    [taskApp] Serializer / View / Route  (#19)
    * bugs
    
    * [taskApp] Serializer / View / Route
    For checking
    
    * Update serializers.py
    
    * Update views.py

--- mlp_api/taskApp/views.py
+++ mlp_api/taskApp/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from taskApp.models import Task
+from taskApp.serializers import TaskSerializer, TaskListSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    TaskViewSet is use for get datas of tasks
+    with considering project_slug
+    """
+
+    serializer_class = TaskSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Task.objects.all()
+        return queryset
+
+    def list(self, *args, **kwargs):
+        queryset = Task.objects.filter(task_project__slug=kwargs.get('project_slug'))
+        serializer = TaskListSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, project_slug, pk=None):
+        queryset = Task.objects.filter(task_project__slug=project_slug)
+        task = get_object_or_404(queryset, pk=pk)
+        serializer = TaskSerializer(queryset, many=True)
+        return Response(serializer.data)
