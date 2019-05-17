140e27a5df3e8bd8e61dd43c2f9528ef7cc764cc
AlTheOne <38474673+AlTheOne@users.noreply.github.com>
2019-05-17 19:12:14 +0300

    Update views.py

--- mlp_api/taskApp/views.py
+++ mlp_api/taskApp/views.py
@@ -1,18 +1,31 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .serializers import *
+
+from taskApp.models import Task
+from taskApp.serializers import TaskSerializer, TaskListSerializer
+
 
 class TaskViewSet(viewsets.ModelViewSet):
+    """
+    TaskViewSet is use for get datas of tasks
+    with considering project_slug
+    """
 
     serializer_class = TaskSerializer
-    serializer_class = TaskProjectSerializer
 
     def get_queryset(self, *args, **kwargs):
         queryset = Task.objects.all()
         return queryset
 
     def list(self, *args, **kwargs):
-        # project_slug записывается в словарь **kwargs
         queryset = Task.objects.filter(task_project__slug=kwargs.get('project_slug'))
-        serializer = self.get_serializer(queryset, many=True)
+        serializer = TaskListSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, project_slug, pk=None):
+        queryset = Task.objects.filter(task_project__slug=project_slug)
+        task = get_object_or_404(queryset, pk=pk)
+        serializer = TaskSerializer(queryset, many=True)
         return Response(serializer.data)
